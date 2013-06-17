65ff81e136c31a942989935b36f6ca3e67a2254c
unknown <okomok@gmail.com>
2013-06-18 01:17:00 +0900

    constant-types interesting

---
+++ core/src/test/scala/pending/ConstantTypeTest.scala
@@ -0,0 +1,59 @@
+
+
+// Copyright Shunsuke Sogame 2008-2013.
+// Distributed under the New BSD license.
+
+
+package com.github.okomoktest; package singtest
+package pending
+
+
+import com.github.okomok.sing
+import sing.makro._
+import junit.framework.Assert._
+import sing.Test
+
+
+import scala.language.existentials
+
+
+class ConstantTypeTest extends org.scalatest.junit.JUnit3Suite {
+
+    def testInt {
+        type ct = ConstantTypeOf.apply(3)
+
+        val x: ct = 3
+        Test.expectError {
+            val x: ct = 4
+        }
+
+        val c: Int = ConstantTermOf.apply[ct]
+        assertEquals(c, 3)
+    }
+
+    def testString {
+        type ct = ConstantTypeOf.apply("he" + "llo")
+        val c: String = ConstantTermOf.apply[ct]
+
+        val x: ct = "hello"
+        Test.expectError {
+            val x: ct = "hi"
+        }
+
+        assertEquals(c, "hello")
+    }
+
+    def testIntExpr {
+        type ct = ConstantTypeOf.apply(1+2)
+
+        val x: ct = 3
+        Test.expectError {
+            val x: ct = 4
+        }
+
+        val c: Int = ConstantTermOf.apply[ct]
+        assertEquals(c, 3)
+    }
+
+}
+
---
+++ macros/src/main/scala/Constant.scala
@@ -0,0 +1,45 @@
+
+
+// Copyright Shunsuke Sogame 2008-2013.
+// Distributed under the New BSD license.
+
+
+package com.github.okomok
+package sing.makro
+
+
+// See:
+//   http://existentialtype.net/2008/07/21/literally-dependent-types/
+//   shapeless.SingletonTypes
+
+
+import scala.language.experimental.macros
+import scala.reflect.macros.Context
+
+
+object ConstantTypeOf {
+    type apply[T](x: T) = macro impl[T]
+
+    def impl[T: c.WeakTypeTag](c: Context)(x: c.Expr[T]): c.Tree = {
+        import c.universe._
+
+        x.tree match {
+            case Literal(c @ Constant(_)) => TypeTree(ConstantType(c))
+            case t => c.abort(c.enclosingPosition, "not constant: " + show(t))
+        }
+    }
+}
+
+
+object ConstantTermOf {
+    def apply[T] = macro impl[T]
+
+    def impl[T: c.WeakTypeTag](c: Context): c.Expr[Any] = {
+        import c.universe._
+
+        weakTypeOf[T].normalize match {
+            case ConstantType(Constant(x)) => c.Expr[Any](Literal(Constant(x)))
+            case t => c.abort(c.enclosingPosition, "not constant: " + show(t))
+        }
+    }
+}
